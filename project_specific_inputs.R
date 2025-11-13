2aef482a7f14f13efd6feb93c78af59ed956a078
jshinb <shin.jihyung@gmail.com>
2025-11-13 12:39:39 -0500

    Initial commit

---
+++ project_specific_inputs.R
@@ -0,0 +1,13 @@
+#************************** Project specific inputs **************************#
+start_date = "2025-11-13"#*["YYYY-MM-DD"]
+modify_date = "2025-11-13"#*["YYYY-MM-DD"]
+analyst = "Jean Shin"#
+
+# Progress
+# 2025-11-13 
+
+# DATA INFORMATION ------------------------------------------------------------#
+# working directory(wd), file names for the brain and non-brain datasets, and missing value code
+#*[directory where the R scripts and data files are stored]
+wd = "~/Library/CloudStorage/OneDrive-Personal/Metabolomic_BrainStructures_ZP"
+#------------------------------------------------------------------------------#
