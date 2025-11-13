281852a16a7a26f666caf507ec246c752d262e52
jshinb <jshinb@users.noreply.github.com>
2025-11-13 12:56:32 -0500

    Update project_specific_inputs.R

--- project_specific_inputs.R
+++ project_specific_inputs.R
@@ -5,9 +5,27 @@ analyst = "Jean Shin"#
 
 # Progress
 # 2025-11-13 
+#* start cleaning up scripts (for better reproducibility)
+#* addressing request of Zdenka:
+#* correlation plots 
+#** (sex-specific: correlation among adjisted and unadjusted brain phenotypes)
 
 # DATA INFORMATION ------------------------------------------------------------#
 # working directory(wd), file names for the brain and non-brain datasets, and missing value code
 #*[directory where the R scripts and data files are stored]
-wd = "~/Library/CloudStorage/OneDrive-Personal/Metabolomic_BrainStructures_ZP"
+#* The following directory is a 'cleaned' directory
+wd = "~/Library/CloudStorage/OneDrive-Personal/Lipidomic_BrainStructures_ZP"
+dir.create(wd)
+
+project.dir = c(data.dir = 'data',
+output.dir = 'outputs',
+script.dir = 'scripts',
+result.dir = 'results')
+names.project.dir = names(project.dir)
+
+project.dir=file.path(wd,project.dir)
+names(project.dir) = names.project.dir
+
+lapply(project.dir,dir.create)
 #------------------------------------------------------------------------------#
+
